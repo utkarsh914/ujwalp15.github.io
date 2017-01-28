e01fc8e3a83381ea7e4e9dedb1120e88a0fd9b98
Bhavarsh Singh <bhavarshs@gmail.com>
2017-01-28 23:14:48 +0530

    Made some modification to php code

--- contact.php
+++ contact.php
@@ -1,10 +1,27 @@
-<?php $name = $_POST['name'];
+<?php 
+
+if(isset($_POST['name'])){
+       
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "ujwalp09@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+
+
+
+if(mail($recipient, $subject, $formcontent, $mailheader)) {
+    echo "Thank You for contacting me, I will get in touch with you shortly!";
+} else {
+   die("Error!");
+}
+
+
+}
+
+
+   
+
 ?>
