4faaac21f7b44c414250e8bae14f3c54b6d8082a
Utkarsh Tiwari <utiwari914@gmail.com>
2017-12-09 19:49:57 +0530

    Fix Email

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "ujwalp09@gmail.com";
+$recipient = "utiwari914@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 
