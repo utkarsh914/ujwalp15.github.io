6d904836fa64f732688e777737773437c28d789f
Utkarsh Tiwari <utiwari914@gmail.com>
2018-12-27 12:03:35 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,9 @@
+function gohomes() {
+	var x = document.getElementsByClassName("closable");
+	for (i=0; i < 1, i++){
+		x[i].style.display = 'none';
+	}
+}
+function gohome() {
+	document.getElementById('dialer-display').style.display='none'
+}
