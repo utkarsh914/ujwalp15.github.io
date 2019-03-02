d515e4056737d4bb14a46a612a34b9eb687e9110
Utkarsh Tiwari <utiwari914@gmail.com>
2019-03-02 06:28:45 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,12 @@
 <?php
 // the message
-$message = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$message = wordwrap($message,70);
-
-// send email
-mail("utiwari914@gmail.com","My subject Test",$message);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "utiwari914@gmail.com";
+$subject = "Contact Form";
+if(mail($recipient, $subject, $formcontent)) {
+    echo "Thank You for contacting me, I will get in touch with you shortly!";
+}
 ?>
