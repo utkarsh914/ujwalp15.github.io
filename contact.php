378789da74d179697fa2ef4699a32aa0c12e056b
Utkarsh Tiwari <utiwari914@gmail.com>
2019-03-03 01:43:43 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-// the message
+/* the message
 $name = $_PUT['name'];
 $email = $_PUT['email'];
 $message = $_PUT['message'];
@@ -9,4 +9,50 @@ $subject = "Contact Form";
 if(mail($recipient, $subject, $formcontent)) {
     echo "Thank You for contacting me, I will get in touch with you shortly!";
 }
+*/
+
+ //PHP Script
+<?php
+require 'PHPMailerAutoload.php';
+if(isset($_POST['send']))
+{
+// Fetching data that is entered by the user
+$email = $_POST['email'];
+$password = $_POST['password'];
+$to_id = $_POST['toid'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+
+// Configuring SMTP server settings
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = $email;
+$mail->Password = $password;
+
+// Email Sending Details
+$mail->addAddress($to_id);
+$mail->Subject = $subject;
+$mail->msgHTML($message);
+
+// Success or Failure
+if (!$mail->send()) {
+$error = "Mailer Error: " . $mail->ErrorInfo;
+echo '<p id="para">'.$error.'</p>';
+}
+else {
+echo '<p id="para">Message sent!</p>';
+}
+}
+else{
+echo '<p id="para">Please enter valid data</p>';
+}
+?>
+
+
+
+
 ?>
